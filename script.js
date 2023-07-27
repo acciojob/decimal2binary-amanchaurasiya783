443c86c6c37927768ebf98a3f9a5895d2502cfb9
amanchaurasiya783 <127719170+amanchaurasiya783@users.noreply.github.com>
2023-07-27 21:13:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+	let decValue = parseInt(num);
+	let binInp = decValue.toString(2);
+	return binInp;
 }
 
 window.decimalToBinary = decimalToBinary;
